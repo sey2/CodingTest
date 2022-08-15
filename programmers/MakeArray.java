da1da6cc5e304b139dd7fd0bd799228a102b4364
sey2 <54762273+sey2@users.noreply.github.com>
2022-08-15 20:38:46 +0900

    자연수 뒤집어 배열 만들기 - level1

---
+++ programmers/MakeArray.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int[] solution(long n) {
+        String str = String.valueOf(n);
+        int[] answer = new int[str.length()];
+        
+        for(int i=str.length()-1; i>=0; i--) 
+            answer[str.length()-i-1] = str.charAt(i)-48;
+
+        return answer;
+    }
+}
