4ec6d12ca31bab08b90cc219ac74f0cb5912277a
sey2 <54762273+sey2@users.noreply.github.com>
2022-08-15 17:59:56 +0900

    [22.08,15] 소수 만들기

---
+++ programmers/MakePrime.java
@@ -0,0 +1,24 @@
+
+class Solution {
+    public int solution(int[] nums) {
+        int answer = 0;
+        
+        for(int i=0; i<nums.length; i++){
+            for(int j=i+1; j<nums.length; j++){
+                for(int k=j+1; k<nums.length; k++){
+                    if(isPrime(nums[i] + nums[j] + nums[k])) answer ++;
+                }
+            }
+        }
+
+        return answer;
+    }
+
+    boolean isPrime(int number) {
+        if(number == 0 || number ==1) return false;
+        for(int i = 2; i <= Math.sqrt(number); i++){
+            if(number % i == 0) return false;
+        }
+        return true;
+    }
+}
