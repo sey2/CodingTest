e1840d8013637297d924ab5ffbe61eed6b2984aa
sey2 <54762273+sey2@users.noreply.github.com>
2022-08-17 03:48:00 +0900

    위장 - level2

---
+++ programmers/Camouflage.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int solution(String[][] clothes) {
+        int answer = 1;
+        HashMap<String, Integer> map = new HashMap<>();
+
+        for(int i=0; i<clothes.length; i++)
+            map.put(clothes[i][1], map.getOrDefault(clothes[i][1],0) + 1);
+
+        for(var num : map.values()){
+            answer *= num+1;
+        }
+
+        return answer-1;
+    }
+}
