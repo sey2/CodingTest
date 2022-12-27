0d2d67d1bafe8ee42c55e8478018e845f761b087
sey2 <casio2978@naver.com>
2022-12-27 20:02:43 +0900

    [Progremmers - Level 2] DefenseGame

---
+++ programmers/DefenseGame.java
@@ -0,0 +1,18 @@
+import java.util.PriorityQueue;
+
+class Solution {
+    public int solution(int n, int k, int[] enemy) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+
+        for(int i=0; i<enemy.length; i++){
+            pq.add(enemy[i]);
+
+            if(pq.size() > k) n -= pq.poll();
+
+            if(n < 0) return i;
+        }
+        
+        return enemy.length;
+    }
+
+}
