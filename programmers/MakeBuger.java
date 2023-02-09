f68f4bf5bf2adc161dcf114d6f2a06e174e0d255
sey2 <casio2978@naver.com>
2023-02-09 21:22:45 +0900

    햄버거 만들기 - level1

---
+++ programmers/MakeBuger.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.Stack;
+
+
+class Solution {
+    public int solution(int[] ingredient) {
+        int answer = 0;
+
+        StringBuilder sb = new StringBuilder("");
+
+        for(int i=0; i<ingredient.length; i++){
+            sb.append(ingredient[i]);
+
+            if(sb.length() > 3 && sb.substring(sb.length()-4, sb.length()).equals("1231")){
+                answer ++;
+                sb = sb.replace(sb.length()-4, sb.length(), "");
+            }
+        }
+
+        return answer;
+    }
+}
