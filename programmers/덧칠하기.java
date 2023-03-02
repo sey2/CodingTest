f9855cbe14c45434e17c5a28c083d8232aced2dc
sey2 <casio2978@naver.com>
2023-03-02 22:09:01 +0900

    Update 덧칠하기.java

--- programmers/덧칠하기.java
+++ programmers/덧칠하기.java
@@ -1,9 +1,7 @@
 class Solution {
     public int solution(int n, int m, int[] section) {
         int answer = 0;
-
-        boolean check[] = new boolean[n+1];
-
+        
         long cur = -1;
         for(int i=0; i<section.length; i++){
             if(section[i] <= cur) continue;
