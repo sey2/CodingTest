21e0071836d5ab1c283fabce3f83b3899fe56b5c
sey2 <casio2978@naver.com>
2023-03-05 23:43:27 +0900

    Delete 프로그래머스/lv0/120894. 영어가 싫어요 directory

--- 프로그래머스/lv0/120894. 영어가 싫어요/영어가 싫어요.java
+++
@@ -1,9 +0,0 @@
-class Solution {
-    String number [] = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
-    public long solution(String numbers) {
-        for(int i=0; i<=9; i++)
-            numbers = numbers.replaceAll(number[i], i + "");
-        
-        return Long.parseLong(numbers);
-    }
-}
