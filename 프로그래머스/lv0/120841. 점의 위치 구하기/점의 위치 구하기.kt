d49f52f80fa5199d21ee903bae1193e91c29214f
sey2 <casio2978@naver.com>
2023-03-16 18:19:12 +0900

    Delete 프로그래머스/lv0/120841. 점의 위치 구하기 directory

--- 프로그래머스/lv0/120841. 점의 위치 구하기/점의 위치 구하기.kt
+++
@@ -1,9 +0,0 @@
-class Solution {
-    fun solution(dot: IntArray) = when{
-        dot[0] < 0 && dot[1] >=0 -> 2
-        dot[0] < 0 && dot[1] < 0 -> 3
-        dot[0] >=0 && dot[1] >=0 -> 1
-        dot[0] >=0 && dot[1] < 0 -> 4
-        else -> 1
-    }
-}
