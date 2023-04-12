73de90821ea8c9cfb9f29cbbd0acd1026d9035f4
sey2 <casio2978@naver.com>
2023-04-12 15:42:22 +0900

    [Silver III] Title: 바이러스, Time: 120 ms, Memory: 14256 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.java
+++ 백준/Silver/2606. 바이러스/바이러스.java
@@ -1,89 +1,52 @@
-import java.util.*;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
-class FastScanner {
-	BufferedReader br;
-	StringTokenizer st;
-	
-	public FastScanner() {
-		br = new BufferedReader(new InputStreamReader(System.in));
-	}
-	
-	String next() {
-		while (st == null || !st.hasMoreElements()) {
-			try {
-				st = new StringTokenizer(br.readLine());
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-		}
-		return st.nextToken();
-	}
-	
-	int nextInt() {
-		return Integer.parseInt(next());
-	}
-	
-	long nextLong() {
-		return Long.parseLong(next());
-	}
-}
+public class Main {
 
-public class Main{	
-	static int n;
-	static int m;
-	static boolean visited[];
-	static int count;
-	static ArrayList<ArrayList<Integer>> computer;
+    static int n, e;
+    static int arr[][];
+    static boolean visited[];
+    static int ans = 0;
 
-    public static void main(String[] args)throws Exception{
-    	FastScanner sc=new FastScanner();
-    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    	BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-    	
-    	// 노드의 수, 정점의  
-    	n = sc.nextInt()+1;
-    	m = sc.nextInt()+1;
-    	
-    	computer = new ArrayList<>();
-    	
-    	for(int i=0; i<n; i++)
-    		computer.add(new ArrayList<Integer>());
-    	
-    	// 양 방향 연결
-    	for(int i=0; i<m-1; i++) {	
-    		int v1 = sc.nextInt();		
-    		int v2 = sc.nextInt();		
-    		
-    		computer.get(v1).add(v2);
-    		computer.get(v2).add(v1);
-    	}
-    	
-    	bfs();
-    	
-    	bw.write(Integer.toString(count));
-    	bw.close();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        n = Integer.parseInt(br.readLine());
+        e = Integer.parseInt(br.readLine());
+
+        arr = new int[n+1][n+1];
+
+        for(int i=0; i<e; i++){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            arr[a][b] = 1;
+            arr[b][a] = 1;
+        }
+
+        visited = new boolean[n+1];
+        visited[1] = true;
+
+        dfs(1);
+        System.out.println(ans);
+
+
     }
-    
-    public static void bfs() {
-    	Queue<Integer> queue = new LinkedList<>();
-    	visited = new boolean[n];
-    	
-		queue.add(1);
-		visited[1] = true;
 
-		while (!queue.isEmpty()) {
-			int tmp = queue.poll();
-			Iterator<Integer> it = computer.get(tmp).iterator();
+    public static void dfs(int s){
 
-			while (it.hasNext()) {
-				int i = it.next();
-				if (!visited[i]) {
-					queue.add(i);
-					visited[i] = true;
-					count ++;
-				}
-			}
-		}
+        for(int i=1; i<=n; i++){
+            if(arr[s][i] > 0 && !visited[i]) {
+                visited[i] = true;
+                dfs(i);
+                ans ++;
+            }
+        }
+
     }
+
 }
