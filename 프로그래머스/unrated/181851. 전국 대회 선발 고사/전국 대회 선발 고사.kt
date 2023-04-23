9da47d06f8641ce2d7512c6d31581c7f91476720
sey2 <casio2978@naver.com>
2023-04-23 20:14:44 +0900

    [unrated] Title: 전국 대회 선발 고사, Time: 4.96 ms, Memory: 58.7 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181851. 전국 대회 선발 고사/전국 대회 선발 고사.kt
@@ -0,0 +1,18 @@
+import java.util.PriorityQueue
+
+class Solution {
+    fun solution(rank: IntArray, attendance: BooleanArray): Int {
+        var answer: Int = 0
+
+        var pq = PriorityQueue<Student>(Comparator { o1, o2 ->  o1.rank - o2.rank})
+
+        for(i in rank.indices){
+            if(attendance[i])
+                pq.add(Student(i, rank[i]))
+        }
+
+        return (10000 * pq.poll().num) + (100 * pq.poll().num) + pq.poll().num
+    }
+}
+
+data class Student(val num: Int, val rank: Int)
