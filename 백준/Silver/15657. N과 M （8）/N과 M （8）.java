98fa0f85ae6bf485221f691083503a9cbfa913f3
sey2 <casio2978@naver.com>
2023-05-27 16:26:38 +0900

    [Silver III] Title: N과 M (8), Time: 228 ms, Memory: 18696 KB -BaekjoonHub

---
+++ 백준/Silver/15657. N과 M （8）/N과 M （8）.java
@@ -0,0 +1,58 @@
+import java.util.*;
+import java.io.*;
+
+
+public class Main
+{
+    static int arr[];
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        st = new StringTokenizer(br.readLine());
+        arr = new int[n];
+        for(int i=0; i< n; i++)
+            arr[i] = Integer.parseInt(st.nextToken());
+
+        Arrays.sort(arr);
+         com(new int[m],0,0, m);
+        // per(new int[m], new boolean[n], 0, m);
+        bw.close();
+    }
+
+    public static void com(int out[], int start, int idx, int r) throws IOException{
+        if(r == 0){
+            for(int i=0; i<out.length; i++){
+                bw.write(out[i] + " ");
+            }
+            bw.write("\n");
+            return;
+        }
+
+        for(int i=start; i<arr.length; i++){
+            out[idx] = arr[i];
+            com(out,  i, idx+1, r - 1);
+        }
+    }
+
+    public static void per(int out[], boolean visited[], int idx, int r) throws IOException{
+        if(r == 0){
+            for(int i=0; i<out.length; i++){
+                bw.write(out[i] + " ");
+            }
+            bw.write("\n");
+            return;
+        }
+
+        for(int i=0; i<arr.length; i++){
+            out[idx] = arr[i];
+            per(out, visited, idx+1, r-1);
+        }
+    }
+
+}
