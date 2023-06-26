d1a98588fe05893d4f89fe2bfbe5477f902e0e3f
sey2 <casio2978@naver.com>
2023-06-27 05:15:22 +0900

    [unrated] Title: n 번째 원소까지, Time: 2.53 ms, Memory: 74.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181889. n 번째 원소까지/n 번째 원소까지.java
@@ -0,0 +1,10 @@
+import java.util.Arrays;
+
+class Solution {
+    public int[] solution(int[] num_list, int n) {
+        return Arrays.stream(num_list)
+                .limit(n)
+                .map(it -> it)
+                .toArray();
+    }
+}
