97248c9c26abca72aa73d4cc7d17ecdba615b06e
sey2 <casio2978@naver.com>
2023-11-16 18:20:15 +0900

    [Gold V] Title: Coins, Time: 136 ms, Memory: 14520 KB -BaekjoonHub

---
+++ 백준/Gold/3067. Coins/Coins.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+
+        for (int t = 0; t < T; t++) {
+            int N = Integer.parseInt(br.readLine());
+            int[] coins = new int[N];
+
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int i = 0; i < N; i++) {
+                coins[i] = Integer.parseInt(st.nextToken());
+            }
+
+            int M = Integer.parseInt(br.readLine());
+            int[] dp = new int[M + 1];
+            dp[0] = 1; 
+
+            for (int i = 0; i < N; i++) {
+                for (int j = coins[i]; j <= M; j++) {
+                    dp[j] += dp[j - coins[i]];
+                }
+            }
+
+            System.out.println(dp[M]);
+        }
+    }
+}
