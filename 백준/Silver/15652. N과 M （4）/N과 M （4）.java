2ef1c06e7d8325f455eb73f4bb91154e2f8a488e
sey2 <casio2978@naver.com>
2023-11-19 08:43:16 +0900

    [Silver III] Title: N과 M (4), Time: 208 ms, Memory: 18840 KB -BaekjoonHub

--- 백준/Silver/15652. N과 M （4）/N과 M （4）.java
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.java
@@ -1,38 +1,40 @@
 import java.io.*;
 import java.util.StringTokenizer;
 
-public class Main {
-    static int arr[];
-    static int end;
-    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+public class Main
+{
+    static BufferedWriter bw;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
+        bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         int n = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
 
-        arr = new int[n];
-        end = m;
+        int arr[] = new int[n];
         for(int i=1; i<=n; i++) arr[i-1] = i;
+        comb(arr, new int[m], 0, 0, m, bw);
 
-        comb(new int[n], 0,0, m);
 
         bw.close();
 
     }
 
-    public static void comb(int out[], int start, int idx, int r) throws IOException {
-        if(r == 0){
-            for(int i=0; i<end; i++)
-                    bw.write(out[i] + " ");
+    public static void comb(int arr[], int out[], int start,  int depth, int r, BufferedWriter bw) throws IOException {
+        if(r == 0) {
+            for(int i=0; i<out.length; i++) {
+                bw.write(out[i] + " ");
+
+            }
             bw.write("\n");
             return;
         }
 
-        for(int i=idx; i<arr.length; i++){
-            out[start] = arr[i];
-            comb(out,start+1, i,r-1);
+        for(int i=start; i<arr.length; i++) {
+            out[depth] = arr[i];
+            comb(arr, out ,i, depth +1 ,r-1, bw);
         }
     }
 
