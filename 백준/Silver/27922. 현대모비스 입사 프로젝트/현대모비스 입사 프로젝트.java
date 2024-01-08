853a4705ebfdef8c4f7065971e543412b7f0c51f
sey2 <casio2978@naver.com>
2024-01-08 15:30:20 +0900

    [Silver III] Title: 현대모비스 입사 프로젝트, Time: 1492 ms, Memory: 69796 KB -BaekjoonHub

--- 백준/Silver/27922. 현대모비스 입사 프로젝트/현대모비스 입사 프로젝트.java
+++ 백준/Silver/27922. 현대모비스 입사 프로젝트/현대모비스 입사 프로젝트.java
@@ -1,49 +1,52 @@
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int N = scanner.nextInt();
-        int K = scanner.nextInt();
-        int[][] lectures = new int[N][3];
+        String in[] = br.readLine().split(" ");
+        int n = Integer.parseInt(in[0]);
+        int k = Integer.parseInt(in[1]);
+        Subject arr[] = new Subject[n];
 
-        for (int i = 0; i < N; i++) {
-            lectures[i][0] = scanner.nextInt(); // 통신기술 역량
-            lectures[i][1] = scanner.nextInt(); // 알고리즘 역량
-            lectures[i][2] = scanner.nextInt(); // 기계구조학 역량
+        for(int i=0; i<n; i++) {
+            in = br.readLine().split(" ");
+            int a = Integer.parseInt(in[0]);
+            int b = Integer.parseInt(in[1]);
+            int c = Integer.parseInt(in[2]);
+            arr[i] = new Subject(a,b,c);
         }
 
-        System.out.println(maxSumOfTwoSkills(N, K, lectures));
+        int ans = 0;
+        Arrays.sort(arr, ((o1, o2) -> o2.arr[0] - o1.arr[0]));
+        ans = Math.max(findMax(k, arr, 0), ans);
+
+        Arrays.sort(arr, ((o1, o2) -> o2.arr[1] - o1.arr[1]));
+        ans = Math.max(findMax(k, arr, 1), ans);
+
+        Arrays.sort(arr, ((o1, o2) -> o2.arr[2] - o1.arr[2]));
+        ans = Math.max(findMax(k, arr, 2), ans);
+        System.out.println(ans);
     }
 
-    public static int maxSumOfTwoSkills(int N, int K, int[][] lectures) {
-        List<Integer> sum12 = new ArrayList<>();
-        List<Integer> sum13 = new ArrayList<>();
-        List<Integer> sum23 = new ArrayList<>();
-
-        for (int[] lecture : lectures) {
-            sum12.add(lecture[0] + lecture[1]);
-            sum13.add(lecture[0] + lecture[2]);
-            sum23.add(lecture[1] + lecture[2]);
-        }
-
-        Collections.sort(sum12, Collections.reverseOrder());
-        Collections.sort(sum13, Collections.reverseOrder());
-        Collections.sort(sum23, Collections.reverseOrder());
-
-        int maxSum12 = sumTopK(sum12, K);
-        int maxSum13 = sumTopK(sum13, K);
-        int maxSum23 = sumTopK(sum23, K);
-
-        return Math.max(Math.max(maxSum12, maxSum13), maxSum23);
-    }
-
-    private static int sumTopK(List<Integer> list, int K) {
+    public static int findMax(int k, Subject subjects[], int j) {
         int sum = 0;
-        for (int i = 0; i < K; i++) {
-            sum += list.get(i);
+        for(int i=0; i<k; i++) {
+            sum += subjects[i].arr[j];
         }
+
         return sum;
+    }
+}
+
+class Subject {
+    int arr[] = new int[3];
+    public Subject(int a, int b, int c) {
+        arr[0] = a + b;
+        arr[1] = b + c;
+        arr[2] = a + c;
     }
 }
