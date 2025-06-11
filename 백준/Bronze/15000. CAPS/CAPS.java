6d94b9b7883ce43473a2890a6e4acb253b1091f0
sey2 <casio2978@naver.com>
2025-06-12 01:37:11 +0900

    [Bronze V] Title: CAPS, Time: 272 ms, Memory: 21056 KB -BaekjoonHub

---
+++ 백준/Bronze/15000. CAPS/CAPS.java
@@ -0,0 +1,12 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        System.out.println(st.nextToken().toUpperCase());
+    }
+}
